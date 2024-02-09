5d5d21ee0af2fef1c993477cc5ff959134b64d45
rrachasak <dev@rachasak.org>
2024-02-09 14:28:23 +0700

    feat: Add Fastwork Link

--- src/routes/consult/+page.svelte
+++ src/routes/consult/+page.svelte
@@ -1,7 +1,12 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Coming soon.</h2>
+		<!-- <h2 class="h2">Coming soon.</h2> -->
+		<a
+			class="btn btn-sm variant-ghost-surface dark:text-white"
+			target="_blank"
+			href="https://fastwork.co/user/itpcckub/legal-33946713"
+		>
+			Fastwork
+		</a>
 	</div>
 </div>
